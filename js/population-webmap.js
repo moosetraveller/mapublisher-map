1e583bb20e6c32d69b49843c2a9f6d84e8714e20
Thomas Zuberbuehler <thomas.zuberbuehler@gmail.com>
2020-02-11 23:43:47 -0400

    apply css to iframe

--- js/population-webmap.js
+++ js/population-webmap.js
@@ -1,4 +1,4 @@
-/*
+/**
  * Changes map in iframe.
  */
 function showMap(map) {
@@ -22,16 +22,28 @@ function showMap(map) {
 
   // artificial 1s busy panel to replace bad flickering with an okay-ish one
   window.setTimeout(() => {
+    applyCssToIFrame();
     $('#mapContainer, #legend').removeClass('hide');
     $('#mapBusyPanel').addClass('hide');
   }, 1000);
 
 }
 
-/*
+/**
  * Updates page to show another map.
  */
 function selectMap(map) {
   window.history.replaceState({'map': map}, map.replace('-', ' '), '?map=' + map);
   showMap(map);
+}
+
+/**
+ * Applies iframe.css to the iframe.
+ */
+function applyCssToIFrame() {
+  var cssLink = document.createElement("link");
+  cssLink.href = "../../css/iframe.css"; 
+  cssLink.rel = "stylesheet"; 
+  cssLink.type = "text/css"; 
+  frames['mapIFrame'].contentDocument.head.appendChild(cssLink);
 }
